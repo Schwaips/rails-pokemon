ede9e20ec2a3237c472822349a795d296db6f90a
Schwaips <charlie.bertrand@live.com>
2025-06-18 19:28:35 +0200

    index pokemons

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,13 @@
+class PokemonsController < ApplicationController
+  def index
+
+    if params[:query].present?
+      @pokemons = Pokemon.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @pokemons = Pokemon.all
+    end
+  end
+
+  def show
+  end
+end
